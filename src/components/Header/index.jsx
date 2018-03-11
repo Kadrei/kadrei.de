79c048ef1af234b02b7a3a0ffdf0f963083cd9eb
Benedikt Kusemann <benedikt.kusemann@klarna.com>
2018-03-11 15:13:59 +0100

    Header Design

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import classnames from 'classnames'
+
+import styles from './index.module.scss'
+
+const navItemClass = classnames(styles.header__nav__item, styles.header__link)
 
 const Header = ({ title }) => (
-  <h1>
-    <Link to="/">{title}</Link>
-  </h1>
+  <header className={styles.spiked}>
+    <div className={styles.header}>
+      <h1 className={styles.header__logo}>
+        <Link to="/" className={styles.header__link}>
+          .k3
+        </Link>
+      </h1>
+      <nav className={styles.header__nav}>
+        <Link to="#about-me" className={navItemClass}>
+          About Me
+        </Link>
+        <Link to="/blog" className={navItemClass}>
+          Blog
+        </Link>
+      </nav>
+    </div>
+  </header>
 )
 
 export default Header
